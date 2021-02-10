711b9e8b30a59bd15fce6754e3d7aaebb48f5ae9
khakan <khakan@khakan.com>
2021-02-10 11:17:55 +0000

    third commit

--- usr/src/index.php
+++ usr/src/index.php
@@ -6,6 +6,6 @@
     </head>
     <body>
         <h1>Ma page web</h1>
-        <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+        <p>khakanous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
     </body>
 </html>
